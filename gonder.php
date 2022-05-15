c2b8fc886b8fe7373e948397f1d44a0781a5dc2b
Mehmet Akif Balcı <96647284+ByAkif@users.noreply.github.com>
2022-05-15 15:29:35 +0300

    Add files via upload

--- gonder.php
+++ gonder.php
@@ -1,9 +1,16 @@
 <?php
-	$ad 	= $_GET["ad"];
-	$soyad 		= $_GET["soyad"];
-	$email 		= $_GET["eposta"];
+	$ad_soyad 	= $_GET["Ad-Soyad"];
+	$email 		= $_GET["email"];
 	$mesaj	 	= $_GET["mesaj"];
 
-	if ($ad==)
-    echo "a büyüktür b";
+$ad_soyad = $_POST['ad'];
+$email = $_POST['email'];
+$mesaj = $_POST['mesaj'];
+
+echo $ad_soyad;
+echo "<br />";
+echo $email;
+echo "<br />";
+echo $mesaj;
+echo "<br />";
 ?>
