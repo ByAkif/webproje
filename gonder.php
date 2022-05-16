e7a05a478b66d351bedac3d2415a049470b10379
Mehmet Akif Balcı <96647284+ByAkif@users.noreply.github.com>
2022-05-16 23:53:57 +0300

    Add files via upload

--- gonder.php
+++ gonder.php
@@ -1,21 +1,21 @@
 <?php
-if(isset($_POST['kadı']) && isset($_POST['sifre'])) { // form gönderilmiş mi
-   $kullanici = $_POST['kadı'];
+ 
+   $kadi = $_POST['kadi'];
    $sifre = $_POST['sifre'];
  
-   if(empty($kadı) || empty($sifre)) { // gönderilenler boş mu
-      echo 'Lütfen boş bırakmayın';
-   } else {
-      if($kullanici == 'b211210075@sakarya.edu.tr' && $sifre == 'b211210075') { // bilgiler doğru mu
-         session_start();
-         $_SESSION['kadı'] = 'b211210075@sakarya.edu.tr';
-         $_SESSION['sifre'] = 'b211210075';
-         echo 'Giriş yaptınız!';
-      } else {
-         echo 'Yanlış kullanıcı adı ya da şifre';
-      }
-   }
-} else {
-   echo 'Lütfen formu kullanın';
-}
+	if ($kadi=="" or $sifre=="")
+	{
+		echo "Lütfen Tüm Alanları Doldurun!";	
+	}
+	else
+	{
+		if ($kadi=="b211210075@sakarya.edu.tr" && $sifre=="b211210075")
+		{
+			echo "Başarılı Şekilde Giriş Yaptınız";	
+		}
+		else
+		{
+			echo "Hatalı Kullanıcı Adı veya Şifre Girdiniz.";	
+		}
+	}
 ?>
